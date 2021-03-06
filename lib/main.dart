b118e642550043f526455c80a4b297919bd2fee1
Solomon Ayodele Ogunbowale <solomonayodeleo@gmail.com>
2021-03-06 10:49:54 +0100

    Minimal Weather App Done - Tutorial

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Weather App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
--- lib/views/homepage.dart
+++ lib/views/homepage.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,23 +10,111 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var API_KEY = "ca2fc75d93296fd8a95aee4ea886026c";
+  var temp;
+  var description;
+  var currently;
+  var humidity;
+  var windSPeed;
+
+  Future getWeather() async {
+    http.Response response = await http.get(
+        'https://api.openweathermap.org/data/2.5/weather?q=London&appid=ca2fc75d93296fd8a95aee4ea886026c');
+    var result = jsonDecode(response.body);
+
+    setState(() {
+      this.temp = result['main']['temp'];
+      this.description = result['weather'][0]['description'];
+      this.currently = result['weather'][0]['main'];;
+      this.humidity = result['main']['humidity'];
+      this.windSPeed = result['wind']['speed'];
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    this.getWeather();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
           Container(
-            height: MediaQuery.of(context).size.height,
+            height: MediaQuery.of(context).size.height / 3,
             width: MediaQuery.of(context).size.width,
             color: Colors.red,
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 Padding(
-                  padding: EdgeInsets.only(),
+                  padding: EdgeInsets.only(bottom: 10),
+                  child: Text(
+                    'Currently in London',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+                Text(
+                  temp != null ? temp.toString() + '\u00B0' : 'Loading',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(bottom: 10),
+                  child: Text(
+                    currently != null ? currently.toString() : 'Loading',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
                 ),
               ],
             ),
           ),
+          Expanded(
+              child: Padding(
+            padding: EdgeInsets.all(20),
+            child: ListView(
+              children: [
+                ListTile(
+                  leading: FaIcon(FontAwesomeIcons.thermometerHalf),
+                  title: Text('Temperatrue'),
+                  trailing: Text(
+                      temp != null ? temp.toString() + '\u00B0' : 'Loading'),
+                ),
+                ListTile(
+                  leading: FaIcon(FontAwesomeIcons.cloud),
+                  title: Text('Weather'),
+                  trailing: Text(
+                      description != null ? description.toString() : 'Loading'),
+                ),
+                ListTile(
+                  leading: FaIcon(FontAwesomeIcons.sun),
+                  title: Text('Humidity'),
+                  trailing:
+                      Text(humidity != null ? humidity.toString() : 'Loading'),
+                ),
+                ListTile(
+                  leading: FaIcon(FontAwesomeIcons.wind),
+                  title: Text('Windspeed'),
+                  trailing: Text(
+                      windSPeed != null ? windSPeed.toString() : 'Loading'),
+                ),
+              ],
+            ),
+          ))
         ],
       ),
     );
